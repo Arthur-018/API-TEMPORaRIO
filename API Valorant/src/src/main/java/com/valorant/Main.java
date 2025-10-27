1cfa826c0ea7d868813731f6f81d8b1386c53d1f
Arthur-018 <toledoarthur11@gmail.com>
2025-10-27 08:22:40 -0300

    Add files via upload

---
+++ API Valorant/src/src/main/java/com/valorant/Main.java
@@ -0,0 +1,25 @@
+package com.valorant;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    private final ValorantConsoleApp menu;
+
+    public Main(ValorantConsoleApp menu) {
+        this.menu = menu;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) {
+        menu.exibir();
+    }
+}
